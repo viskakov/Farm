abd88f19a7ab88067a438b1785359e77fa8873b5
Vsevolod Iskakov <vsev0l0d.vsevolod@gmail.com>
2022-12-05 23:37:21 +0300

    Added GameStatView

---
+++ Assets/_Scripts/UI/GameStatView.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+namespace Farm._Scripts
+{
+    public class GameStatView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _carrotLabel;
+        [SerializeField] private TextMeshProUGUI _expLabel;
+    }
+}
