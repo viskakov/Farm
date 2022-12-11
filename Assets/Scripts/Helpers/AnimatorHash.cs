1c72732b9092940de41bc36de14003292fb9b8bd
Vsevolod Iskakov <vsev0l0d.vsevolod@gmail.com>
2022-12-11 19:01:47 +0300

    Added hash for pickup animation

--- Assets/Scripts/Helpers/AnimatorHash.cs
+++ Assets/Scripts/Helpers/AnimatorHash.cs
@@ -6,5 +6,6 @@ namespace Farm.Helpers
     {
         public static readonly int Walking = Animator.StringToHash("Walking");
         public static readonly int Plant = Animator.StringToHash("Plant");
+        public static readonly int Pickup = Animator.StringToHash("Pickup");
     }
 }
