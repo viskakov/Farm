fce504816c3ccf6cb1a8d302d73499b43c1d95c5
Vsevolod Iskakov <vsev0l0d.vsevolod@gmail.com>
2022-12-29 18:01:56 +0300

    Added screenshot tool

---
+++ Assets/Editor/TakeScreenshot.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Farm.Editor
+{
+    public static class Screenshot
+    {
+        [MenuItem("My Tools/Take Screenshot")]
+        public static void TakeScreenshot()
+        {
+            var path = Path.Combine(Application.dataPath, "Screenshots");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            var dateTimeNow = DateTime.Now.ToString("MM-dd-yyyy (HH-mm-ss)");
+            ScreenCapture.CaptureScreenshot(Path.Combine(path, $"Screenshot_{dateTimeNow}.png"), 2);
+            AssetDatabase.Refresh();
+
+            Debug.Log("Screenshot captured!");
+        }
+    }
+}
